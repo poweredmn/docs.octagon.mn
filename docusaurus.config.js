b6139a21a42f2a44dee778ddad646a633bde85f6
janubis <r.javkhlan@gmail.com>
2022-08-25 13:43:03 +0800

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -3,21 +3,21 @@
 
 /** @type {import('@docusaurus/types')} */
 const config = {
-  title: 'Octagon DOCS',
-  tagline: 'Монгол гэрээс Метаверст хамтдаа',
-  url: 'https://docs.octagon.mn',
+  title: "Octagon DOCS",
+  tagline: "Монгол гэрээс Метаверст хамтдаа",
+  url: "https://docs.octagon.mn",
   trailingSlash: false,
-  baseUrl: '/',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
-  organizationName: 'powerdmn', // Usually your GitHub org/user name.
-  projectName: 'docs.octagon.mn', // Usually your repo name.
-  titleDelimiter: ' | ', // Defaults to `|`
+  baseUrl: "/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.ico",
+  organizationName: "poweredmn", // Usually your GitHub org/user name.
+  projectName: "docs.octagon.mn", // Usually your repo name.
+  titleDelimiter: " | ", // Defaults to `|`
 
-  presets: require('./config/presets'),
-  themeConfig: require('./config/theme'),
-  plugins: require('./config/plugin'),
+  presets: require("./config/presets"),
+  themeConfig: require("./config/theme"),
+  plugins: require("./config/plugin"),
 };
 
 module.exports = config;
