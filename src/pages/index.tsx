0305f08943b50e7c456997b8a60b83a619885d85
Orgyldinio <orgyldinio@proton.me>
2022-10-12 15:23:48 +0800

    introjs түр байхгүй болгов
    introjs түр байхгүй болгов

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,51 +1,51 @@
-import React from "react";
-import Layout from "@theme/Layout";
-import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
-import LayoutTw from "@site/src/theme/LayoutTw";
-import "intro.js/introjs.css";
-import { Steps, Hints } from "intro.js-react";
-import Announcement from "../components/Homepage/Announcement";
-import Hero from "../components/Homepage/Hero";
-import Club from "../components/Homepage/Club";
-import Collections from "../components/Homepage/Collections";
-import Posts from "../components/Homepage/Posts";
-import Staking from "../components/Homepage/Staking";
+import React from 'react';
+import Layout from '@theme/Layout';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import LayoutTw from '@site/src/theme/LayoutTw';
+import 'intro.js/introjs.css';
+import { Steps, Hints } from 'intro.js-react';
+import Announcement from '../components/Homepage/Announcement';
+import Hero from '../components/Homepage/Hero';
+import Club from '../components/Homepage/Club';
+import Collections from '../components/Homepage/Collections';
+import Posts from '../components/Homepage/Posts';
+import Staking from '../components/Homepage/Staking';
 
 export default function Home() {
   //https://github.com/HiDeoo/intro.js-react
   const { siteConfig } = useDocusaurusContext();
   const steps = [
     {
-      element: ".highlightClass-0",
-      intro: "Хөгжүүлэлттэй холбоотой постууд",
-      position: "left",
+      element: '.highlightClass-0',
+      intro: 'Хөгжүүлэлттэй холбоотой постууд',
+      position: 'left',
     },
     {
-      element: ".highlightClass-1",
-      intro: "Крипто ертөнцийн эргэн тойронд",
-      position: "top",
+      element: '.highlightClass-1',
+      intro: 'Крипто ертөнцийн эргэн тойронд',
+      position: 'top',
     },
     {
-      element: ".highlightClass-2",
-      intro: "Хэвлэл мэдээллийн сувгуудаар цацагдсан манай контентууд",
-      position: "top",
+      element: '.highlightClass-2',
+      intro: 'Хэвлэл мэдээллийн сувгуудаар цацагдсан манай контентууд',
+      position: 'top',
     },
     {
-      element: ".highlightClass-3",
-      intro: "Тайлбар бичлэгүүд",
-      position: "top",
+      element: '.highlightClass-3',
+      intro: 'Тайлбар бичлэгүүд',
+      position: 'top',
     },
     {
-      element: ".highlightClass-4",
-      intro: "Хэрэглэгчиддээ зориулсан бүх төрлийн сургалтууд",
-      position: "right",
+      element: '.highlightClass-4',
+      intro: 'Хэрэглэгчиддээ зориулсан бүх төрлийн сургалтууд',
+      position: 'right',
     },
   ];
   const hints = [
     {
-      element: ".highlightClass-0",
-      hint: "Хөгжүүлэлтийн долоо хоног бүрийн мэдээлэл энд орж байх болно.",
-      hintPosition: "top-right",
+      element: '.highlightClass-0',
+      hint: 'Хөгжүүлэлтийн долоо хоног бүрийн мэдээлэл энд орж байх болно.',
+      hintPosition: 'top-right',
     },
   ];
   return (
@@ -62,7 +62,7 @@ export default function Home() {
             <Collections />
           </section>
           <Posts />
-          <Steps
+          {/* <Steps
             enabled={true}
             steps={steps}
             initialStep={0}
@@ -73,7 +73,7 @@ export default function Home() {
             enabled={true}
             hints={hints}
             ref={(steps) => (this.steps = steps)}
-          />
+          /> */}
           <Staking />
         </main>
       </LayoutTw>
